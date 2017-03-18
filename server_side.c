3f88a18065316177a1fa28f3c632dd2f36d104ae
ioanantal <antal.micky@yahoo.com>
2017-03-18 23:04:57 +0200

    auth6

--- server_side.c
+++ server_side.c
@@ -17,14 +17,27 @@ int current_number_of_users = 0;
 
 int *connections;
 struct sockaddr_in server_addr, client_addr;
+
+message create_message(char *type, char *username, unsigned char length, char *body);
+
+void authentication(int sock, char *message_to_send, message *message_received);
+
 int socket_desc, client_sock, socket_length, *new_sock;
 message *client_auth;
 char data[PACK_SIZE];
 
-typedef struct userList{
+
+typedef struct userList {
     char username[4];
     char password[100];
 } userList;
+
+
+userList existing_users[] = {
+        {"user\0", "123"},
+        {"admi\0", "12"},
+        {"gigi\0", "asds"}
+};
 
 userList connected_users[NUMBER_OF_USERS];
 
@@ -71,13 +84,13 @@ void *connection_handler(void *socket_desc) {
     int sock = *(int *) socket_desc;
     int read_size;
     char buffer_from_clients[MAX_BUFFER_SIZE];
+    char *message_to_send;
 
     //Receive a message from client
     while (1) {
         memset(buffer_from_clients, 0, MAX_BUFFER_SIZE);
         message message_received;
         if ((read_size = (int) recv(sock, buffer_from_clients, TOTAL_HEADER_LEN, 0)) > 0) {
-            //Send the message back to
             printf("Message received from cliend id: %d, %s\n", sock, buffer_from_clients);
             message_received = deserialize(buffer_from_clients);
         }
@@ -87,31 +100,71 @@ void *connection_handler(void *socket_desc) {
         }
 
         if (strcmp(message_received.type, "AUT") == 0) {
-            printf("Authenticated userId: %s\n", message_received.username);
-            for(int i = 0; i < current_number_of_users; i++){
-                if(strcmp(connected_users[i].username, message_received.username) != 0) {
-                    strcpy(connected_users[current_number_of_users].username, message_received.username);
-                    strcpy(connected_users[current_number_of_users].password, message_received.body);
-                    current_number_of_users++;
-                }
-            }
-            //TODO check user in list and send back a ACK/ERR message
+            //printf("Authenticated userId: %s\n", message_received.username);
+            authentication(sock, message_to_send, &message_received);
         } else if (strcmp(message_received.type, "MSG") == 0) {
             for (int index = 0; index < current_number_of_users; index++) {
                 if (connections[index] > 0) {
                     printf("Client: %d\n", connections[index]);
-                    char *message_to_send;
+
                     message_to_send = serialize(message_received);
                     //TODO format string to a desired structure
                     write(connections[index], message_to_send, strlen(message_to_send));
                 }
             }
         } else if (strcmp(message_received.type, "DIS") == 0) {
+            message ack_disconnect = create_message("DIS", message_received.username, '1', " ");
+            message_to_send = serialize(ack_disconnect);
+            write(sock, message_to_send, strlen(message_to_send));
             printf("Log out\n");
 
         }
 
     }
+}
+
+void authentication(int sock, char *message_to_send, message *message_received) {
+    int flag = 0; //error
+    //checks if user is in the existing list of users
+    for (int i = 0; i < 3; i++) {
+        if ((strncmp(existing_users[i].username, (*message_received).username, 4) == 0)
+            && (strcmp(existing_users[i].password, (*message_received).body) == 0)) {
+            flag = 1;
+            break;
+
+        }
+    }
+    if (flag == 1) {
+        //checks if there is any other user connected with the same username
+        for (int i = 0; i < current_number_of_users; i++) {
+            if (strncmp(connected_users[i].username, (*message_received).username, 4) == 0) {
+                flag = 0;
+            }
+        }
+    }
+    if (flag == 1) {
+        strcpy(connected_users[current_number_of_users].username, (*message_received).username);
+        strcpy(connected_users[current_number_of_users].password, (*message_received).body);
+        current_number_of_users++;
+        //TODO send ack
+        message ack_success_msg = create_message("ACK", (*message_received).username, 0, "");
+        message_to_send = serialize(ack_success_msg);
+    } else {
+        //TODO send error msg to the client
+        message ack_error_msg = create_message("ERR", (*message_received).username, 0, "");
+        message_to_send = serialize(ack_error_msg);
+    }
+    write(sock, message_to_send, strlen(message_to_send));
+}
+
+message create_message(char *type, char *username, unsigned char length, char *body) {
+    message msg;
+    strcpy(msg.type, type);
+    strcpy(msg.username, username);
+    msg.length = length;
+    msg.body = calloc(length, sizeof(char));
+    strcpy(msg.body, body);
+    return msg;
 }
 
 
@@ -140,7 +193,7 @@ int main(int argc, char *argv[]) {
         }
 
         puts("Handler assigned");
-        current_number_of_users++;
+        //current_number_of_users++;
     }
 
     if (client_sock < 0) {
